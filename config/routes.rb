2f73a2d941e38a0797944e77bd30a86c5cd2c34e
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-28 09:50:19 +0300

    add items to list routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :todo_items
-  resources :todo_lists
+  resources :todo_lists do
+    resources :todo_items
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
