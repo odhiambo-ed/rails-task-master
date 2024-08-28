4c633da0417b535731431c26d3e6212513d53ebe
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-08-28 09:49:56 +0300

    edit todo items migration

--- db/migrate/20240828064024_create_todo_items.rb
+++ db/migrate/20240828064024_create_todo_items.rb
@@ -3,7 +3,7 @@ class CreateTodoItems < ActiveRecord::Migration[7.0]
     create_table :todo_items do |t|
       t.belongs_to :todo_list, null: false, foreign_key: true
       t.string :text
-      t.boolean :completed
+      t.boolean :completed, default: false
 
       t.timestamps
     end
